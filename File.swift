234a0d365cd0f1e75818f908b18d4d24bd9e528e
Mykhailo <miketeamgamer13@gmail.com>
2025-12-10 07:35:22 -0600

    card class

---
+++ File.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  InscryptionMJ
+//
+//  Created by MYKHAILO NAUMOV on 12/10/25.
+//
+
+import SwiftUI
+
+struct Card: Identifiable {
+    
+    let id = UUID()
+    let emoji: String
+    var isFaceUp = false
+    var isMatched = false
+    
+    init(emoji: String, isFaceUp: Bool = false, isMatched: Bool = false) {
+        
+        self.emoji = emoji
+        self.isFaceUp = isFaceUp
+        self.isMatched = isMatched
+        
+    }
+    
+}
