a845db94ab88602607c6a063a8fbd5036901e146
Mykhailo <miketeamgamer13@gmail.com>
2025-12-11 08:09:33 -0600

    Before the end of class

--- ContentView.swift
+++ ContentView.swift
@@ -3,7 +3,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TabView {
-            MainView()
+            OptionsView()
                 .tabItem {
                     Label("Home", systemImage: "house")
                 }
